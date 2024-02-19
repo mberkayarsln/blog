ec2b4f4b99e53c0cb5d165246cf0d3a8fc80f256
mberkayarsln <mberkayarslann@gmail.com>
2024-02-20 00:23:43 +0300

    homepage design completed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,28 @@
+import Image from "next/image";
+import styles from "./home.module.css";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creative Thought Agency.</h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid,
+          ipsum quibusdam adipisci sapiente suscipit culpa?
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image src="/brands.png" alt="" fill className={styles.brandImg} />
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image src="/hero.gif" alt="" fill className={styles.heroImg} />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
