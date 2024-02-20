a8f3f9eceae7bf08ffb04224fea0eadf607a9d21
mberkayarsln <mberkayarslann@gmail.com>
2024-02-21 00:32:45 +0300

    contact page design completed

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,5 +1,35 @@
+import styles from "./contact.module.css";
+import Image from "next/image";
+
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          className={styles.img}
+          src="/contact.png"
+          alt="Contact image"
+          fill
+        />
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type="text" placeholder="Name and Surname" required />
+          <input type="email" placeholder="Email Address" required />
+          <input type="number" placeholder="Phone Number (Optional)" />
+          <textarea
+            name=""
+            id=""
+            cols={30}
+            rows={10}
+            placeholder="Message"
+            required
+          ></textarea>
+          <button>Send</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ContactPage;
